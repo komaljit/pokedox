c3024ddbc09ff39badd33707f664f239ebf875e1
komaljit <komaljit1874@gmail.com>
2018-09-02 18:06:13 -0700

    added the pokemon API file

--- src/components/api.js
+++ src/components/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 const API = 'https://cors.io/?http://pokeapi.co/api/v2';
 
-export const fetchPokemonDeatils = (pokemon) =>{
+export const fetchPokemonDetails = (pokemon) =>{
     console.log(`${API}/pokemon/${pokemon}`);
-    axios.get(`${API}/pokemon/${pokemon}`)
+    axios.get(`${API}/${this.state.url}/${pokemon}`)
         .then((res) => {
-            return res})
+            return res.data})
         .catch(error => {
             console.log("This is error");
             return error;
